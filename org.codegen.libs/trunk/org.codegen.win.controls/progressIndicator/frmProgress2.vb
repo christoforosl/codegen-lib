924947ff4d16bcbb0d81713d8378c3ba26c9fca2
chris <chris@cc33f4f5-c82a-ce84-02ca-8259bb3be96e>
2014-04-15 16:23:51 +0000

    new, simpler progress form

---
+++ org.codegen.libs/trunk/org.codegen.win.controls/progressIndicator/frmProgress2.vb
@@ -0,0 +1,67 @@
+﻿Namespace BackroundProgressIndicator
+
+    Public Class frmProgress2
+
+        Public Event ProgressCancelled(ByVal sender As Object, ByVal e As EventArgs)
+
+        Public Sub setShowCancelButton(ByVal showit As Boolean)
+
+            Me.btnCancel.Visible = showit
+
+        End Sub
+
+        Public Sub setMessageLabelText(ByVal text As String)
+
+            Me.lblMessage.Text = text
+
+        End Sub
+
+        Private Sub frmProgress_Load(ByVal sender As Object, _
+                                     ByVal e As System.EventArgs) Handles Me.Load
+
+            Me.btnCancel.Text = WinControlsLocalizer.getString("cmdCancel")
+
+        End Sub
+
+
+        Private Sub btnCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) _
+                    Handles btnCancel.Click
+
+            Dim restoreTopMost As Boolean
+
+            Try
+                restoreTopMost = Me.TopMost
+                If Me.TopMost Then
+                    Me.TopMost = False
+                End If
+                If winUtils.MsgboxQuestion(WinControlsLocalizer.getString("cancel_progress")) = vbYes Then
+
+                    RaiseEvent ProgressCancelled(Me, New EventArgs())
+
+                End If
+
+            Finally
+                Me.TopMost = restoreTopMost
+            End Try
+
+        End Sub
+
+
+        Public Sub ReportProgress(ByVal currentStep As Integer, ByVal msg As String)
+
+            If currentStep > Me.ProgressBar.Maximum Then currentStep = Me.ProgressBar.Maximum
+            If currentStep < Me.ProgressBar.Minimum Then currentStep = Me.ProgressBar.Minimum
+
+            Me.ProgressBar.Value = currentStep
+            Me.lblPercentage.Text = currentStep & "%"
+
+            If String.IsNullOrEmpty(msg) = False Then
+                Me.lblMessage.Text = msg
+            End If
+            Me.Refresh()
+
+        End Sub
+
+    End Class
+
+End Namespace
