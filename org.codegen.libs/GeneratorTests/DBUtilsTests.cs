1e2cb0c931d4b9f93ce7ceaf53a3cefe28949529
christoforosl <christoforosl of netu.com.cy>
2015-05-31 08:19:47 +0300

    new DBUtils tests

---
+++ org.codegen.libs/GeneratorTests/DBUtilsTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using org.model.lib.db;
+
+namespace GeneratorTests {
+
+	[TestClass]
+	public class DBUtilsTests {
+
+		
+		[TestMethod]
+		public void testParsePositionalParamsForSQLServer() {
+
+			string x;
+
+			x = DBUtils.Current().replaceParameterPlaceHolders("Telephone=? and Telephone=? and ?=x");
+			Assert.AreEqual(x, "Telephone=@0 and Telephone=@1 and @2=x");
+
+			x = DBUtils.Current().replaceParameterPlaceHolders("where Telephone=? and Telephone=?", "x", "y");
+			Assert.AreEqual(x, "where Telephone=@0 and Telephone=@1");
+
+			x = DBUtils.Current().replaceParameterPlaceHolders("where Telephone=? and Telephone=?", "x", "y");
+			Assert.AreEqual(x, "where Telephone=@0 and Telephone=@1");
+
+			x = DBUtils.Current().replaceParameterPlaceHolders("where Telephone=? and Telephone=?", "x", "y");
+			Assert.AreEqual(x, "where Telephone=@0 and Telephone=@1");
+
+			x = DBUtils.Current().replaceParameterPlaceHolders("where Telephone=? and Telephone=? and ?=x");
+			Assert.AreEqual(x, "where Telephone=@0 and Telephone=@1 and @2=x");
+
+			x = DBUtils.Current().replaceParameterPlaceHolders("where Telephone BETWEEN ? and ?");
+			Assert.AreEqual(x, "where Telephone BETWEEN @0 and @1");
+
+
+		}
+
+	}
+}
