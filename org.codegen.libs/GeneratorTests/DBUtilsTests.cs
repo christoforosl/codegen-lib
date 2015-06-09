425d1eb1a4b7f4d3fa9944f59e508bdc101e1cf4
christoforosl <christoforosl>
2015-06-09 14:43:09 +0300

    fix count test

--- org.codegen.libs/GeneratorTests/DBUtilsTests.cs
+++ org.codegen.libs/GeneratorTests/DBUtilsTests.cs
@@ -18,12 +18,17 @@ namespace GeneratorTests {
 		[TestMethod]
 		public void testPagedList() {
 
+            int x = DBUtils.Current().getIntValue("select count(*) from employee");
+            if (x > 5) {
+                x = 5;
+            }
+
 			using (DataContext ctx = DBUtils.Current().dbContext()) {
 
-				var query = ctx.ExecuteQuery<Employee>(@"SELECT Employeeid,EmployeeName FROM employee").Skip(1).Take(5);
+				var query = ctx.ExecuteQuery<Employee>(@"SELECT Employeeid,EmployeeName FROM employee").Skip(1).Take(x);
 				var lst = query.ToList();
 
-				Assert.AreEqual(lst.Count, 5, "Expected to receive 5 employee records");
+				Assert.AreEqual(lst.Count, (x-1), "Expected to receive " + (x-1) +" employee records, got: " + lst.Count);
 				string output = JsonConvert.SerializeObject(lst);
 				System.Diagnostics.Debug.WriteLine(output);
 			}
