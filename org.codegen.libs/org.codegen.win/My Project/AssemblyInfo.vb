c4d84a81026f7dcef773b760466b7cb2b4d2a6f5
christoforosl <christoforosl>
2015-06-11 14:55:06 +0300

    removed "public" from autogenerated partial class

--- org.codegen.libs/org.codegen.win/My Project/AssemblyInfo.vb
+++ org.codegen.libs/org.codegen.win/My Project/AssemblyInfo.vb
@@ -31,5 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.0.9.0")> 
-<Assembly: AssemblyFileVersion("4.0.9.0")> 
+<Assembly: AssemblyVersion("4.0.9.2")> 
+<Assembly: AssemblyFileVersion("4.0.9.2")> 
+
