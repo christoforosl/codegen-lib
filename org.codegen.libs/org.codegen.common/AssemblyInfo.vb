352a64ce0123a52f055d38b603e54d1671adfb68
christoforosl <christoforosl of netu.com.cy>
2015-07-15 07:40:15 +0300

    release of win controls w/o specific version dependency

--- org.codegen.libs/org.codegen.common/AssemblyInfo.vb
+++ org.codegen.libs/org.codegen.common/AssemblyInfo.vb
@@ -28,6 +28,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("4.0.0.1")> 
-
-<Assembly: AssemblyFileVersionAttribute("4.0.9.3")> 
+<Assembly: AssemblyVersion("4.0.0.2")> 
+<Assembly: AssemblyFileVersionAttribute("4.0.0.2")> 
