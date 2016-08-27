5beb51afeffc212d24c876dd1d0277704463d89d
christoforosl <christoforosl of netu.com.cy>
2016-08-27 16:15:39 +0300

    release if win libs

--- org.codegen.libs/org.codegen.win.controls/AssemblyInfo.vb
+++ org.codegen.libs/org.codegen.win.controls/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.0.2.35")> 
-<Assembly: AssemblyFileVersion("3.0.2.35")> 
+<Assembly: AssemblyVersion("3.0.2.37")> 
+<Assembly: AssemblyFileVersion("3.0.2.37")> 
 
