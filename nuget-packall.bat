040711155348295652141d5c7b869694a430e728
christoforosl08 <christoforosl08@gmail.com>
2019-07-25 16:54:58 +0300

    updated package release

--- nuget-packall.bat
+++ nuget-packall.bat
@@ -1,9 +1,27 @@
 @echo off
-cd %USERPROFILE%\VSProjects\codegen-lib-LastEditionOfIIC
+cd %~dp0 
+REM THE ABOVE CHANGES CURRENT DIR TO BATCH FILE'S DIR
+REM %USERPROFILE%\VSProjects\codegen-lib-LastEditionOfIIC
 
 rem call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-set NUGETEXE="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\hflum5rs.rt1\~PC\ProjectTemplates\CSharp\NuGet\NuGet.exe"
-set OUTPUTDIR=%USERPROFILE%\VSProjects\Nuget.Local
+set NUGETEXE=%USERPROFILE%\VSProjects\NuGet.exe
+
+if NOT exist %NUGETEXE% (
+	ECHO NUGETEXE DOES NOT EXIST! DOWNLOAD FROM https://www.nuget.org/downloads AND PUT IN IN "%USERPROFILE%\VSProjects\NuGet.exe"
+	EXIT /B 1
+)
+
+
+set NUGET_LOCAL_REPOSITORY=%USERPROFILE%\VSProjects\Nuget.Local
+if NOT exist %NUGET_LOCAL_REPOSITORY% (
+	MKDIR %NUGET_LOCAL_REPOSITORY%
+)
+if NOT exist %NUGET_LOCAL_REPOSITORY% (
+	ECHO %NUGET_LOCAL_REPOSITORY% DOES NOT EXIST! CREATE IN "%NUGET_LOCAL_REPOSITORY%"
+	EXIT /B 1
+)
+
+
 set SOLUTIONDIR=%USERPROFILE%\VSProjects\IIC\packages
 
 CALL :dopackahe org.codegen.common.3.1
@@ -18,8 +36,8 @@ EXIT /B %ERRORLEVEL%
 :dopackahe
 echo "------start %~1 --"
 rd /s /q %SOLUTIONDIR%\%~1
-del %OUTPUTDIR%\%~1.nupkg
-%NUGETEXE% pack %~1.nuspec -OutputDirectory %OUTPUTDIR%
+del %NUGET_LOCAL_REPOSITORY%\%~1.nupkg
+%NUGETEXE% pack %~1.nuspec -OutputDirectory %NUGET_LOCAL_REPOSITORY%
 echo "------end %~1 --"
 rem ECHO The square of %~1 is %~2
 rem PAUSE
